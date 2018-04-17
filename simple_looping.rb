4f510752f94584bc3e02382312a8f8eaca1efa56
Faraz Hanif <hfaraz745@gmail.com>
2018-04-17 21:45:14 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -9,7 +9,7 @@ def loop_iterator(number_of_times)
         break
       end
     end
-end
+
 
 # def times_iterator(number_of_times)
 #   phrase = "Welcome to Flatiron School's Web Development Course!"
